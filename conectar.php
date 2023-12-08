992b3878c2ce04a5b451108118aa74a6ce16ee00
ChristopherM51 <christophermatheus1@hotmail.com>
2023-12-08 20:33:14 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -8,7 +8,7 @@ $nomeBanco = 'sa3pwfe';
 // Conexão com o banco de dados
 $conexao = new mysqli($host, $dbUsuario, $dbSenha, $nomeBanco);
 
-if($conexao->connect_error) {[[[]]]
+if($conexao->connect_error) {[[[jjjjj]]]
     die("Erro na conexão com o banco de dados: ".$conexao->connect_error);
 }
 ?>
