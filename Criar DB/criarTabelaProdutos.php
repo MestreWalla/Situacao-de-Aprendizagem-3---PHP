2feaf0f6a94b6365e307cfa2570b66a20c1b0241
ChristopherM51 <christophermatheus1@hotmail.com>
2023-12-08 21:47:12 -0300

    1

--- Criar DB/criarTabelaProdutos.php
+++ Criar DB/criarTabelaProdutos.php
@@ -15,10 +15,10 @@ if ($conn->connect_error) {
 // Cria a tabela produtos
 $sql_produtos = "CREATE TABLE produtos (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    img VARCHAR(255),
-    nome VARCHAR(50) NOT NULL,
+    imgProduto VARCHAR(255),
+    nomeProduto VARCHAR(50) NOT NULL,
     descricao TEXT,
-    valor DECIMAL(10,2) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
     tag1 VARCHAR(30),
     tag2 VARCHAR(30),
     tag3 VARCHAR(30)
